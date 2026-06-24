afce4b53289afb82727daa3589b9d5dda31a8e7b
KozyaSkyworker <d_koz@inbox.ru>
2026-06-24 08:37:55 +0400

    install vue; clear project

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template >
+  <div class="wrapper">
+    <header class="header">
+   header
+  </header>
+
+  <main>
+    main
+  </main>
+  <footer>footer</footer>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
